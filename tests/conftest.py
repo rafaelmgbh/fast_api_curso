82982f5b43d428eb29d120f88c65bb90c397080d
Rafael Santos <rafaelsantosdearaujo@gmail.com>
2024-10-09 17:19:08 -0300

    [Aula 04 ] : ADicionando Alembic e SQL alchemy e configurações iniciais dos mesmos .

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,23 @@
 import pytest
-from starlette.testclient import TestClient
+from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
 
 from fast_api_curso.app import app
+from fast_api_curso.models import table_registry
 
 
 @pytest.fixture
 def client():
     return TestClient(app)
+
+
+@pytest.fixture
+def session():
+    engine = create_engine("sqlite:///:memory:")
+    table_registry.metadata.create_all(engine)
+
+    with Session(engine) as session:
+        yield session
+
+    table_registry.metadata.drop_all(engine)
